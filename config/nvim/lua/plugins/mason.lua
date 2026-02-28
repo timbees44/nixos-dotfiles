5f581b57b6c0c1f56d6299358b29a83951f70e7b
tim <tburrey@protonmail.com>
2026-02-28 21:31:47 +0000

    moving dotfile configs to dotfiles away from home.nix

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,21 +1,44 @@
+local servers = {
+  "html",
+  "cssls",
+  "tailwindcss",
+  "lua_ls",
+  "graphql",
+  "emmet_ls",
+  "pyright",
+}
+
+local function auto_install_enabled()
+  if vim.g.mason_auto_install ~= nil then
+    return vim.g.mason_auto_install
+  end
+
+  local env = vim.env.MASON_AUTO_INSTALL
+  if env then
+    env = env:lower()
+    if env == "1" or env == "true" or env == "yes" then
+      return true
+    elseif env == "0" or env == "false" or env == "no" then
+      return false
+    end
+  end
+
+  return false
+end
+
 return {
   {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      -- list of servers for mason to install
-      ensure_installed = {
-        "html",
-        "cssls",
-        "tailwindcss",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "pyright",
-      },
-    },
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      local enable_auto = auto_install_enabled()
+      opts = opts or {}
+      opts.ensure_installed = enable_auto and servers or {}
+      opts.automatic_installation = enable_auto
+      return opts
+    end,
     dependencies = {
       {
-        "mason-org/mason.nvim",
+        "williamboman/mason.nvim",
         opts = {
           ui = {
             icons = {
