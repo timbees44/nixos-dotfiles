d15d7fe4f75fd34a0f48c4caec9562219d9eeaf2
tim <github.jacket490@passinbox.com>
2026-03-08 10:10:44 +0000

    fixing darwin and adding comments

--- homes/macbook.nix
+++ homes/macbook.nix
@@ -1,9 +1,12 @@
 { config, pkgs, lib, doomemacs, ... }:
 let
+  # Dotfiles repo checkout location on macOS.
   dotfiles = "${config.home.homeDirectory}/projects/nixos-dotfiles/config";
   create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+  # Doom framework install destination.
   doomDir = "${config.home.homeDirectory}/.emacs.d";
 
+  # Config directories to expose under ~/.config via symlinks.
   configs = {
     doom = "doom";
     nvim = "nvim";
@@ -15,8 +18,10 @@ in
 {
   home.username = "tim";
   home.homeDirectory = "/Users/tim";
+  # Home Manager compatibility baseline.
   home.stateVersion = "24.05";
 
+  # GPG agent setup suitable for macOS key prompts.
   programs.gpg.enable = true;
   services.gpg-agent = {
     enable = true;
@@ -24,6 +29,7 @@ in
     pinentry.package = pkgs.pinentry_mac;
   };
 
+  # User-facing packages managed by Home Manager.
   home.packages = with pkgs; [
     bat
     btop
@@ -55,6 +61,7 @@ in
     })
   ];
 
+  # Keep shell startup files sourced from the repo.
   home.file.".bashrc" = {
     source = create_symlink "${dotfiles}/bash/.bashrc";
   };
@@ -63,6 +70,17 @@ in
     source = create_symlink "${dotfiles}/bash/.bash_profile";
   };
 
+  # Provide legacy Doom path expected by some setups/tools.
+  home.file.".doom.d" = {
+    source = create_symlink "${dotfiles}/doom";
+  };
+
+  # Keep Doom CLI and Emacs aligned on the same config location.
+  home.sessionVariables = {
+    DOOMDIR = "${config.home.homeDirectory}/.config/doom";
+  };
+
+  # Create ~/.config/* links from the `configs` attrset above.
   xdg.configFile = builtins.mapAttrs
     (name: subpath: {
       source = create_symlink "${dotfiles}/${subpath}";
@@ -70,6 +88,7 @@ in
     })
     configs;
 
+  # Install/update Doom core files on each activation.
   home.activation.doomInstall = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     doomSrc=${lib.escapeShellArg doomemacs}
     doomDest=${lib.escapeShellArg doomDir}
@@ -83,6 +102,7 @@ in
     done
   '';
 
+  # Sync Doom modules/packages after core files are in place.
   home.activation.doomSync = lib.hm.dag.entryAfter [ "doomInstall" ] ''
     doomBin="${doomDir}/bin/doom"
     if [ -x "$doomBin" ]; then
