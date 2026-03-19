f39a32c592399e9ee7750b8a7f5195a01ec60403
tim <github.jacket490@passinbox.com>
2026-03-19 09:11:52 +0000

    restore frigate openvino model config

--- modules/homelab/default.nix
+++ modules/homelab/default.nix
@@ -192,6 +192,14 @@ in {
             type: openvino
             device: CPU
 
+        model:
+          width: 300
+          height: 300
+          input_tensor: nhwc
+          input_pixel_format: bgr
+          path: /openvino-model/ssdlite_mobilenet_v2.xml
+          labelmap_path: /openvino-model/coco_91cl_bkgr.txt
+
         record:
           enabled: true
           retain:
