e9cf2e501cd07280ebfdda71b98ededa1418f65c
tim <github.jacket490@passinbox.com>
2026-03-24 08:40:15 +0000

    fixing scenes bug

--- modules/homelab/default.nix
+++ modules/homelab/default.nix
@@ -180,12 +180,6 @@ in {
 
     systemd.services.home-assistant.serviceConfig.SupplementaryGroups = [ "video" "render" ];
 
-    systemd.tmpfiles.rules = [
-      "f /var/lib/hass/automations.yaml 0600 hass hass -"
-      "f /var/lib/hass/scripts.yaml 0600 hass hass -"
-      "f /var/lib/hass/scenes.yaml 0600 hass hass -"
-    ];
-
     environment.etc = {
       "frigate/config.yaml".text = ''
         mqtt:
@@ -277,6 +271,9 @@ in {
     };
 
     systemd.tmpfiles.rules = [
+      "f /var/lib/hass/automations.yaml 0600 hass hass -"
+      "f /var/lib/hass/scripts.yaml 0600 hass hass -"
+      "f /var/lib/hass/scenes.yaml 0600 hass hass -"
       (ensureDir cfg.mediaDir cfg.user "media" "0755")
       (mediaSubdirRule "movies")
       (mediaSubdirRule "tvshows")
