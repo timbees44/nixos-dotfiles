f5cd65d2e1b7ad1d296b4eba024d4fc0299cbaef
tim <tim>
2026-04-18 14:34:06 +0100

    config update

--- config/doom/custom.el
+++ config/doom/custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ai-code-onboarding-seen t)
  '(custom-safe-themes
    '("4d714a034e7747598869bef1104e96336a71c3d141fa58618e4606a27507db4c"
      "19d62171e83f2d4d6f7c31fc0a6f437e8cec4543234f0548bad5d49be8e344cd" default))
