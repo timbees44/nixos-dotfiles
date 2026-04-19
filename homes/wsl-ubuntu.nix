cc910693d57909bc0b4281603a378f42665eb77e
tim <tim>
2026-04-19 18:26:31 +0100

    legion no doom -> kick

--- homes/wsl-ubuntu.nix
+++ homes/wsl-ubuntu.nix
@@ -1,11 +1,10 @@
-{ config, pkgs, lib, doomemacs, ... }:
+{ config, pkgs, lib, ... }:
 let
   dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
   createSymlink = path: config.lib.file.mkOutOfStoreSymlink path;
-  doomDir = "${config.home.homeDirectory}/.emacs.d";
 
   configs = {
-    doom = "doom";
+    emacs = "emacs-kick";
     nvim = "nvim";
     starship = "starship";
     tmux = "tmux";
@@ -70,12 +69,8 @@ in
     source = createSymlink "${dotfiles}/zsh/.zprofile";
   };
 
-  home.file.".doom.d" = {
-    source = createSymlink "${dotfiles}/doom";
-  };
-
-  home.sessionVariables = {
-    DOOMDIR = "${config.home.homeDirectory}/.config/doom";
+  home.file.".emacs.d/init.el" = {
+    source = createSymlink "${dotfiles}/emacs-kick/init.el";
   };
 
   xdg.configFile = builtins.mapAttrs
@@ -84,30 +79,4 @@ in
       recursive = true;
     })
     configs;
-
-  home.activation.doomInstall = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    doomSrc=${lib.escapeShellArg doomemacs}
-    doomDest=${lib.escapeShellArg doomDir}
-    mkdir -p "$doomDest"
-    ${pkgs.rsync}/bin/rsync -a --delete \
-      --chmod=Du+rwx,Fu+rw \
-      --exclude='.local/' \
-      "$doomSrc"/ "$doomDest"/
-    for d in .local .local/etc .local/cache .local/state; do
-      install -d -m 700 "$doomDest/$d"
-    done
-  '';
-
-  home.activation.doomSync = lib.hm.dag.entryAfter [ "doomInstall" "linkGeneration" ] ''
-    doomBin="${doomDir}/bin/doom"
-    straightFile="${doomDir}/.local/straight/repos/straight.el/straight.el"
-    if [ -x "$doomBin" ]; then
-      export DOOMDIR="${config.home.homeDirectory}/.config/doom"
-      export PATH=${lib.makeBinPath [ pkgs.emacs pkgs.git pkgs.gnutar pkgs.gzip pkgs.coreutils ]}:$PATH
-      if [ ! -f "$straightFile" ]; then
-        "$doomBin" install --force || true
-      fi
-      "$doomBin" sync || true
-    fi
-  '';
 }
